4a79b83d8c350f351962c57c41b91cc8f4ffa735
Jonghan Park <jonghan.park@edu.sait.ca>
2022-07-07 11:19:40 -0600

    Update card css

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Card({ post }) {
   return (
-    <div>
+    <div className="card">
       <span className="title">{post.title}</span>
       <img src={post.img} alt="img" className="img" />
       <p className="desc">{post.desc}</p>
