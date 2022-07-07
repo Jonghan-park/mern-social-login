9b53fd099cedfaa1436a24044a74dc04016185bb
Jonghan Park <jonghan.park@edu.sait.ca>
2022-07-07 13:30:57 -0600

    Update Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Post from "./pages/Post";
 import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Login />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/post/:id" element={<Post />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
