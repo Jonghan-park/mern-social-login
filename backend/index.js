76a4b73b55ec8009b3ae231d401967d18f80df2c
Jonghan Park <jonghan.park@edu.sait.ca>
2022-07-08 01:09:38 -0600

    Create backend

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const cookieSession = require("cookie-session");
+const express = require("express");
+const passport = require("passport");
+const app = express();
+const cors = require("cors");
+
+app.use(
+  cookieSession({ name: "session", keys: ["lama"], maxAge: 24 * 60 * 60 * 100 })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,POST,PUT,DELETE",
+    Credential: true,
+  })
+);
+
+app.listen("5000", () => {
+  console.log("Server is running");
+});
